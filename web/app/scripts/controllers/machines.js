eb715333ff58628b361c1aec8ee0a3fd9a1955a4
Al Johri <al.johri@gmail.com>
2015-04-14 23:04:17 -0500

    add locations

--- web/app/scripts/controllers/machines.js
+++ web/app/scripts/controllers/machines.js
@@ -8,10 +8,16 @@
  * Controller of the webApp
  */
 angular.module('webApp')
-  .controller('MachinesCtrl', function ($scope, Ref, $firebaseArray) {
+  .controller('MachinesCtrl', function ($scope, $window, Ref, $firebaseArray) {
+
+  // $scope.$window.$scope = $scope;
+  // console.log($scope.$eval('$window'));
 
 	$scope.machines = $firebaseArray(Ref.child('machines'));
 	$scope.machines.$loaded().catch(alert);
+
+  $scope.locations = $firebaseArray(Ref.child('locations'));
+  $scope.locations.$loaded().catch(alert);
 
   $scope.showRuns = true;
 
@@ -26,6 +32,33 @@ angular.module('webApp')
     var changed_machine = snapshot.val();
     $scope.last_change = changed_machine;
   });
+
+  $scope.averageMachineTime = function(type) {
+
+    var waitTimes = _.chain($scope.machines)
+      .filter(function(machine) {return machine['type'] == type; })
+      .map(function(machine) {return _.values(machine.runs); }).flatten(true)
+      .map(function(run) {return run ? (run[1] - run[0])/1000/60 : undefined; })
+      .compact()
+      .value();
+
+    return _.reduce(waitTimes, function(x,y) {return x+y; }, 0) / waitTimes.length;
+
+  }
+
+  $scope.averageLocationTime = function(location_name, type) {
+
+    var waitTimes = _.chain($scope.machines)
+      .filter(function(machine) {return machine['location_name'] == location_name; })
+      .filter(function(machine) {return machine['type'] == type; })
+      .map(function(machine) {return _.values(machine.runs); }).flatten(true)
+      .map(function(run) {return run ? (run[1] - run[0])/1000/60 : undefined; })
+      .compact()
+      .value();
+
+    return _.reduce(waitTimes, function(x,y) {return x+y; }, 0) / waitTimes.length;
+
+  }
 
   $scope._ = _;
 
--- web/app/scripts/filters/reverse.js
+++ web/app/scripts/filters/reverse.js
@@ -6,3 +6,23 @@ angular.module('webApp')
       return angular.isArray(items)? items.slice().reverse() : [];
     };
   });
+
+
+angular.module('webApp')
+	.filter('total', ['$parse', function ($parse) {
+    return function (input, property) {
+        var i = input instanceof Array ? input.length : 0,
+            p = $parse(property);
+
+        if (typeof property === 'undefined' || i === 0) {
+            return i;
+        } else if (isNaN(p(input[0]))) {
+            throw 'filter total can count only numeric values';
+        } else {
+            var total = 0;
+            while (i--)
+                total += p(input[i]);
+            return total;
+        }
+    };
+}]);
